72f946ab5d76050cc5844cb890c88b4442343789
angela413li <angela413li@gmail.com>
2020-11-19 14:38:01 +0800

    Level 4 game 1st draft

---
+++ lv4scipt.js
@@ -0,0 +1,46 @@
+/*synonym*/
+
+function enter()
+{
+
+var myString = "restore";
+var newString = mystring.replace(/a|e|i|o|u/g, "*")
+
+var myString = "reserve";
+var newString = mystring.replace(/a|e|i|o|u/g, "*")
+
+var myString = "wither";
+var newString = mystring.replace(/a|e|i|o|u/g, "*")
+
+var myString = "scarlet";
+var newString = mystring.replace(/a|e|i|o|u/g, "*")
+
+var myString = "corporation";
+var newString = mystring.replace(/a|e|i|o|u/g, "*")
+
+var myString = "ground";
+var newString = mystring.replace(/a|e|i|o|u/g, "*")
+
+var myString = "astonished";
+var newString = mystring.replace(/a|e|i|o|u/g, "*")
+}
+
+/*antonym*/
+
+
+function feedback()
+{
+	var message = "";
+	
+	if ()
+	{
+		message = "Correct!";
+	}
+	
+	else
+	{
+		message = "Try again!";
+	}
+	
+	document.getElementById("feedback").innerHTML = message;
+}
