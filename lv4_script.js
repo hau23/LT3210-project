8049ffd1316b0e0e2f2f5e486c08f58bc3bb2d1b
angela413li <angela413li@gmail.com>
2020-11-22 18:07:16 +0800

    Lv4 game 4th draft update style

---
+++ lv4_script.js
@@ -0,0 +1,162 @@
+/*synonyms*/
+
+function q1_input()
+{
+	var q1_feedbackElement = document.getElementById("q1_feedback");
+	
+	if (q1_answer.value == "restore")
+	{
+		q1_feedbackElement.innerHTML = "You are correct!";
+	}
+	else
+	{
+		q1_feedbackElement.innerHTML = "Try again!";
+	}
+	
+	/*clear previous answer*/
+	document.getElementById("q1_answer").value = "";
+}
+
+function q2_input()
+{
+	var q2_feedbackElement = document.getElementById("q2_feedback");
+	
+	if (q2_answer.value == "reserve")
+	{
+		q2_feedbackElement.innerHTML = "You are correct!";
+	}
+	else
+	{
+		q2_feedbackElement.innerHTML = "Try again!";
+	}
+	
+	document.getElementById("q2_answer").value = "";
+}
+
+function q3_input()
+{
+	var q3_feedbackElement = document.getElementById("q3_feedback");
+	
+	if (q3_answer.value == "wither")
+	{
+		q3_feedbackElement.innerHTML = "You are correct!";
+	}
+	else
+	{
+		q3_feedbackElement.innerHTML = "Try again!";
+	}
+	
+	document.getElementById("q3_answer").value = "";
+}
+
+function q4_input()
+{
+	var q4_feedbackElement = document.getElementById("q4_feedback");
+	
+	if (q4_answer.value == "scarlet")
+	{
+		q4_feedbackElement.innerHTML = "You are correct!";
+	}
+	else
+	{
+		q4_feedbackElement.innerHTML = "Try again!";
+	}
+	
+	document.getElementById("q4_answer").value = "";
+}
+
+function q5_input()
+{
+	var q5_feedbackElement = document.getElementById("q5_feedback");
+	
+	if (q5_answer.value == "corporation")
+	{
+		q5_feedbackElement.innerHTML = "You are correct!";
+	}
+	else
+	{
+		q5_feedbackElement.innerHTML = "Try again!";
+	}
+	
+	document.getElementById("q5_answer").value = "";
+}
+
+function q6_input()
+{
+	var q6_feedbackElement = document.getElementById("q6_feedback");
+	
+	if (q6_answer.value == "ground")
+	{
+		q6_feedbackElement.innerHTML = "You are correct!";
+	}
+	else
+	{
+		q6_feedbackElement.innerHTML = "Try again!";
+	}
+	
+	document.getElementById("q6_answer").value = "";
+}
+
+function q7_input()
+{
+	var q7_feedbackElement = document.getElementById("q7_feedback");
+	
+	if (q7_answer.value == "astonished")
+	{
+		q7_feedbackElement.innerHTML = "You are correct!";
+	}
+	else
+	{
+		q7_feedbackElement.innerHTML = "Try again!";
+	}
+	
+	document.getElementById("q7_answer").value = "";
+}
+
+function q8_input()
+{
+	var q8_feedbackElement = document.getElementById("q8_feedback");
+	
+	if (q8_answer.value == "friend")
+	{
+		q8_feedbackElement.innerHTML = "You are correct!";
+	}
+	else
+	{
+		q8_feedbackElement.innerHTML = "Try again!";
+	}
+	
+	document.getElementById("q8_answer").value = "";
+}
+
+function q9_input()
+{
+	var q9_feedbackElement = document.getElementById("q9_feedback");
+	
+	if (q9_answer.value == "confront")
+	{
+		q9_feedbackElement.innerHTML = "You are correct!";
+	}
+	else
+	{
+		q9_feedbackElement.innerHTML = "Try again!";
+	}
+	
+	document.getElementById("q9_answer").value = "";
+}
+
+function q10_input()
+{
+	var q10_feedbackElement = document.getElementById("q10_feedback");
+	
+	if (q10_answer.value == "depart")
+	{
+		q10_feedbackElement.innerHTML = "You are correct!";
+	}
+	else
+	{
+		q10_feedbackElement.innerHTML = "Try again!";
+	}
+	
+	document.getElementById("q10_answer").value = "";
+}
